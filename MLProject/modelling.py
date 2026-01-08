091468a47636faa0e115d18541b217e2b25286a5
ErwinHafiz <Erwinhafizzxr@gmail.com>
2026-01-08 23:36:47 +0700

    revision on modelling.py, trigger new

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,9 +3,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import mlflow
+import dagshub
 
 
+# INIT DAGSHUB + MLFLOW
+dagshub.init(
+    repo_owner="erwinhafizzxr",
+    repo_name="titanic-mlflow-erwin-hafiz-triadi",
+    mlflow=True
+)
 
+
+# LOAD DATA
 df = pd.read_csv("dataset_preprocessing/titanic_clean.csv")
 
 X = df.drop("2urvived", axis=1)
@@ -15,12 +24,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+mlflow.autolog()
+# MLFLOW EXPERIMENT
+# mlflow.set_experiment("Titanic-Baseline")
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+# with mlflow.start_run():
+model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
-preds = model.predict(X_test)
-acc = accuracy_score(y_test, preds)
-mlflow.log_metric("accuracy", acc)
-mlflow.sklearn.log_model(model, "model")
-print(f"Model accuracy: {acc}")
-print("done bang")
+# preds = model.predict(X_test)
+# acc = accuracy_score(y_test, preds)
+# LOGGING
+# mlflow.log_metric("accuracy", acc)
+# mlflow.sklearn.log_model(model, "model")
